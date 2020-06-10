d521c6e4e36625da59d54829121685dd1720befe
A-F-V <faracea2000@gmail.com>
2020-06-10 15:20:11 +0100

    Set port to 1611

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const m = require('./mongoServerTest');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const bookRouter = express.Router();
 bookRouter.route('/books').get((req, res) => {
