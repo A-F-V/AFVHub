e33f46a33d93ac6c6f7c1b95c58e7c80a6c3086c
A-F-V <faracea2000@gmail.com>
2020-06-13 18:27:26 +0100

    Big progress, forwards message to clustered bullet

--- bin/microservices/bullet.js
+++ bin/microservices/bullet.js
@@ -1,5 +1,9 @@
 const cluster = require('cluster');
 const os = require('os');
+const express = require('express');
+
+const app = express();
+const port = 1612;
 
 class Bullet {
   constructor() {
@@ -11,13 +15,20 @@ class Bullet {
   }
 }
 
-
+let bullet;
 if (cluster.isMaster) {
   const size = os.cpus().length;
+
   Bullet.setup().then((s) => {
+    bullet = s;
     for (let x = 0; x < size; x++) {
       cluster.fork();
     }
   });
+  Object.values(cluster.workers).forEach((worker) => { worker.bullet = bullet; });
 } else {
+  app.get('/', (req, res) => {
+    res.send(`Handled by${process.pid} Done with Bullet`);
+  });
+  app.listen(port);
 }
--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,4 @@
-// const express = require('express');
-// const app = express();
-// const port = process.env.PORT || 1611;
+
 /*
 const bookRouter = express.Router();
 bookRouter.route('/books').get((req, res) => {
@@ -19,8 +17,32 @@ app.listen(port, () => {
 // microservice.send({ type: 'start', param: `${__dirname}/microservices/bullet` });
 // microservice.send({ type: 'test' });
 // microservice2.send({ type: 'test' });
-
+const cluster = require('cluster');
+const os = require('os');
+const express = require('express');
+const http = require('http');
 const MicroserviceManager = require('./microservices/msm');
 
-const msm = new MicroserviceManager(`${__dirname}/microservices`, './config/microservices.txt');
-msm.setup();
+const app = express();
+const port = process.env.PORT || 1611;
+let msm;
+
+if (cluster.isMaster) {
+  msm = new MicroserviceManager(`${__dirname}/microservices`, './config/microservices.txt');
+  msm.setup();
+  const size = os.cpus().length;
+  for (let x = 0; x < size; x++) {
+    cluster.fork();
+  }
+  Object.values(cluster.workers).forEach((worker) => { worker.msm = msm; });
+} else {
+  console.log('TESt');
+  app.get('/', (req, res) => {
+    if (req.headers.type === 'bullet') {
+      http.get({ hostname: 'localhost', port: 1612 }, (r) => {
+        r.on('data', (data) => { res.send(data.toString()); });
+      });
+    }
+  });
+  app.listen(port);
+}
